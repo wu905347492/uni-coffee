7101047a742b9f4937fb6dca8eb8fbd49f83ef09
yujie <yujie.wu@d1m.cn>
2022-06-22 00:03:52 +0800

    test

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,22 +1,6 @@
 <template>
   <view>
     <view>{{ page }}</view>
-    <ids-button @click="handleClick" :preventClick="true">Dialog</ids-button>
-    <ids-dialog :show="modal" @click="handleClick" title="提示" content="确定退出登录吗？"></ids-dialog>
-    <view class="btn-box"><ids-button shape="circle" shadow @click="bDrawer">从底部弹出 Drawer</ids-button></view>
-    <ids-drawer mode="bottom" :visible="bottomDrawer" @close="closeDrawer">
-      <view class="tui-drawer__box"
-        ><tui-button height="80rpx" type="green" shape="circle" @click="closeDrawer">关闭抽屉</tui-button></view
-      >
-    </ids-drawer>
-    <view>
-      <!-- <u-popup :show="show" @close="close" @open="open">
-        <view>
-          <text>出淤泥而不染，濯清涟而不妖</text>
-        </view>
-      </u-popup>
-      <u-button @click="show = true">打开</u-button> -->
-    </view>
   </view>
 </template>
 
